8343c01c37cf9385b06efd691a19e8e93e2078e2
WANG <wangqianlf@live.com>
2024-03-20 13:17:59 +0100

    tabControl

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,6 +47,15 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -56,6 +65,9 @@
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -165,7 +177,7 @@
             this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.button7, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(67, 463);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 389);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
@@ -173,7 +185,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(748, 306);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(696, 243);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button6
@@ -181,7 +193,7 @@
             this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button6.Location = new System.Drawing.Point(3, 3);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(243, 96);
+            this.button6.Size = new System.Drawing.Size(226, 74);
             this.button6.TabIndex = 0;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
@@ -190,16 +202,16 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(3, 105);
+            this.listBox1.Location = new System.Drawing.Point(3, 83);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.Size = new System.Drawing.Size(120, 68);
             this.listBox1.TabIndex = 2;
             // 
             // pictureBox2
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.pictureBox2, 2);
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(252, 105);
+            this.pictureBox2.Location = new System.Drawing.Point(235, 83);
             this.pictureBox2.Name = "pictureBox2";
             this.tableLayoutPanel1.SetRowSpan(this.pictureBox2, 2);
             this.pictureBox2.Size = new System.Drawing.Size(376, 143);
@@ -210,9 +222,9 @@
             // button7
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.button7, 2);
-            this.button7.Location = new System.Drawing.Point(252, 3);
+            this.button7.Location = new System.Drawing.Point(235, 3);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(493, 66);
+            this.button7.Size = new System.Drawing.Size(458, 66);
             this.button7.TabIndex = 1;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
@@ -223,9 +235,9 @@
             this.panel2.Controls.Add(this.button9);
             this.panel2.Controls.Add(this.button8);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 207);
+            this.panel2.Location = new System.Drawing.Point(3, 163);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(243, 96);
+            this.panel2.Size = new System.Drawing.Size(226, 77);
             this.panel2.TabIndex = 4;
             // 
             // button8
@@ -255,11 +267,107 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(751, 357);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(398, 391);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button10);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(390, 362);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Onglet 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(390, 362);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Onglet 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(63, 77);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(152, 81);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(390, 362);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Onglet 3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(523, 675);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(196, 73);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Supprimer l\'onglet 3";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(247, 677);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(196, 71);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "Ajouter un nouvel onglet";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(3, 675);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(214, 83);
+            this.button13.TabIndex = 6;
+            this.button13.Text = "Supprimer tous les onglets";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1210, 807);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -277,6 +385,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -301,6 +413,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,8 @@ namespace Container_notes_cours
         public Form1()
         {
             InitializeComponent();
+            //tabControl1.SelectedIndex = 2;
+            tabControl1.SelectedTab = tabPage2;
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -27,6 +29,28 @@ namespace Container_notes_cours
             btn1.BackColor = Color.Green;
             flowLayoutPanel1.Controls.Add(btn1);
 
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Vous avez appuyé sur le bouton de l'onglet 1";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            tabControl1.Controls.Remove(tabPage3);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            TabPage tp1 = new TabPage("Onglet 4");
+            tp1.BackColor = Color.Red;
+            tabControl1.Controls.Add(tp1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            tabControl1.Controls.Clear();
         }
     }
 }
